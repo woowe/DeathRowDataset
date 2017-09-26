0278b8f5cbef5db121744d5f25c7506e4feb0f57
Jason Cardinal <jason.cardinal@candeocreative.com>
2017-09-26 17:09:55 -0500

    Download and normalize images

--- download_images.js
+++ download_images.js
@@ -1,11 +1,57 @@
 const fs = require('fs'),
     http = require('http'),
-    cheerio = require('cheerio')
-    downloadImage = require('download-image');
+    cheerio = require('cheerio'),
+    download = require('image-downloader');
 
+const rx = require('rxjs/Rx');
+const Observable = require('rxjs/Observable').Observable;
+const Subject = require('rxjs/Subject').Subject;
+
+    
 let images_urls = JSON.parse(fs.readFileSync('./image_urls.json'));
 
-for(let src of images_urls) {
-    let file = src.substring(src.lastIndexOf('/')).substring(1);
-    downloadImage(src, './Data/' + file);
-}
+let retry_urls = [];
+
+// async function downloadIMG(url, dest) {
+//     try {
+//         const { filename, image } = await download.image({url, dest});
+//         console.log(filename);
+//     } catch (e) {
+
+//         throw e;
+//     }
+// }
+
+let dest = './Data/images/';
+let $done = new Subject();
+let $urls = Observable.interval(250)
+    .switchMap(() => Observable.of(images_urls))
+    .takeUntil($done)
+    .map(urls => urls.pop());
+    // .switchMap(url => {
+    //     return Observable.fromPromise(download.image({url, dest}))
+    //         .do(({filename, image}) => console.log('File saved to', filename))
+    //         .catch(err => { throw err });
+    // })
+
+$urls.subscribe(url => {
+    // console.log(images_urls.length, ' +++ ', url);
+
+    download.image({ url, dest })
+        .then(({filename, image}) => {
+            console.log("File saved to", filename);
+        })
+        .catch((err) => {
+            throw err
+        });
+
+    if(images_urls.length === 0 && retry_urls.length === 0) {
+        $done.next(true);
+    }
+});
+
+
+
+// for(let src of images_urls) {
+
+// }
--- normalize_images.js
+++ normalize_images.js
@@ -4,8 +4,8 @@ const fs = require('fs'),
       imgSize = require('image-size');
 
 let opts = {
-    'IMAGE_SIZE': 256,
-    'DATASET_PATH': './Wiki Art Data/'
+    'IMAGE_SIZE': 512,
+    'DATASET_PATH': './Data/images/'
 };
 
 let normalize_path = opts.DATASET_PATH + 'normalized_' + opts.IMAGE_SIZE;
@@ -33,18 +33,46 @@ console.log('Creating normalize directory... ', normalize_path);
 fs.mkdirSync(normalize_path);
 
 let images = fs.readdirSync(opts.DATASET_PATH)
-                            .filter((v, i) => (v.endsWith('.jpg') || v.endsWith('.jpeg') || v.endsWith('.png')))
-                            .map(file_name => ({file_name, 'size': imgSize(opts.DATASET_PATH + file_name)}));
+    .filter((v, i) => (v.endsWith('.jpg') || v.endsWith('.jpeg') || v.endsWith('.png')))
+    .map(file_name => ({file_name}));
 
-Rx.Observable.from(images)
-    .mergeMap((image_info) =>
-        Rx.Observable.fromPromise(jimp.read(opts.DATASET_PATH + image_info.file_name)),
-        (image_info, image) => ({image_info, image})
-    )
-    .subscribe(({image_info, image}) => {
-        // let size = imgSize(opts.DATASET_PATH + image_file_name);
-        console.log('Resizing ', image_info.file_name, ' to ', opts.IMAGE_SIZE, ' x ', opts.IMAGE_SIZE);
-        image.resize(opts.IMAGE_SIZE, opts.IMAGE_SIZE)
-            .quality(100)
-            .write(normalize_path + '/' + image_info.file_name);
-    })
+let $normalize_images = Rx.Observable.create((observer) => {
+    if(images.length === 0) {
+        console.log('Complete');
+        observer.complete({image_info: null});
+    }
+
+    let image_info = images.pop();
+
+    jimp.read(opts.DATASET_PATH + image_info.file_name)
+        .then(image => {
+            if(image.bitmap.width >= opts.IMAGE_SIZE && image.bitmap.height >= opts.IMAGE_SIZE) {
+                console.log('Resizing... ', image_info.file_name, ' +++ ', image.bitmap.width, 'x', image.bitmap.height);
+                console.log('Saving to...', normalize_path + '/' + image_info.file_name);
+                image.cover(opts.IMAGE_SIZE, opts.IMAGE_SIZE)
+                    .quality(100)
+                    .write(normalize_path + '/' + image_info.file_name);
+            }
+            observer.next({image_info});
+        })
+        .catch(err => console.log(image_info.file_name, '\nError ', err))
+});
+
+$normalize_images
+    // .last()
+    .take(images.length)
+    .subscribe(({image_info}) => {
+        // console.log('Finished!');
+    });
+
+// Rx.Observable.interval(1000) 
+//     .filter(i => i < images.length)
+//     .map(i => images[i])
+//     .mergeMap(image_info =>
+//         Rx.Observable.fromPromise(
+//           jimp.read(opts.DATASET_PATH + image_info.file_name)),
+//         (image_info, image) => ({image_info, image})
+//     )
+//     .subscribe(({image_info, image}) => {
+        
+//     })
